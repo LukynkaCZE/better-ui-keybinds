de90f09731ab92a4e47d1b6a5c7e921c82ad4734
LukynkaCZE <jiraklukas058@seznam.cz>
2025-02-23 15:19:58 +0100

    update to version 1.21.4

--- src/main/java/cz/lukynka/betteruikeybinds/client/mixin/CreativeModeTabsMixin.java
+++ src/main/java/cz/lukynka/betteruikeybinds/client/mixin/CreativeModeTabsMixin.java
@@ -1,21 +1,24 @@
 package cz.lukynka.betteruikeybinds.client.mixin;
 
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static net.minecraft.world.item.CreativeModeTabs.SEARCH;
-
 @Mixin(CreativeModeTabs.class)
 public class CreativeModeTabsMixin {
 
+    @Shadow @Final public static ResourceKey<CreativeModeTab> SEARCH;
+
     @Inject(at = @At("HEAD"), method = "getDefaultTab", cancellable = true)
     private static void getDefaultTab(CallbackInfoReturnable<CreativeModeTab> cir) {
-        cir.setReturnValue(BuiltInRegistries.CREATIVE_MODE_TAB.getOrThrow(SEARCH));
+        cir.setReturnValue(BuiltInRegistries.CREATIVE_MODE_TAB.getValueOrThrow(SEARCH));
     }
 
 }
